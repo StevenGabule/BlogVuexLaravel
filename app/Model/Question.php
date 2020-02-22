327624000038f113cc12a09dd96c6a161b5983a1
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-02-22 23:01:54 +0800

    implement the relation of every model

---
+++ app/Model/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
