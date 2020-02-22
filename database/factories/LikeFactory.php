adffc766c7bbaa1510ebe637836bc3e7c8b629c6
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-02-22 23:23:13 +0800

    implement the factory in every models and generate some dummy data

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -1,9 +1,12 @@
 <?php
 
 use Faker\Generator as Faker;
-
-$factory->define(App\Model\Like::class, function (Faker $faker) {
+use App\Model\Like;
+use App\User;
+$factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,8 +2,18 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Model\Reply::class, function (Faker $faker) {
+use App\Model\Reply;
+use App\Model\Question;
+use App\User;
+
+$factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function() {
+            return Question::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
