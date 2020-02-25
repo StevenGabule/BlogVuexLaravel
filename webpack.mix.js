110b7958858c3e72b506ccee5c0a5e334e7b94ff
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-02-26 06:15:02 +0800

    install the vuetifyjs for frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// webpack.config.js
+
+mix.js("resources/assets/js/app.js", "public/js").sass(
+    "resources/assets/sass/app.scss",
+    "public/css"
+);
